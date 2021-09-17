1d75d69a5130e36673a9d3a0395b43203c75d4f6
Natalya Pletneva <nntalita@gmail.com>
2021-09-17 23:41:18 +0300

    optimization

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,13 @@ public class Main {
         CreditCalculatorService service = new CreditCalculatorService();
         double percentPerYear = 9.99;
 
-
         long creditPaymetn1Year = service.calculate(1_000_000, percentPerYear, 1 * 12);
         System.out.println("ежемесячный платеж по кредиту на 1 000 000 руб. сроком на 1 год " + creditPaymetn1Year + " руб под " + percentPerYear + "% годовых");
 
-        long creditPaymetn2Year = service.calculate(1_000_000, 9.99,  2 * 12);
+        long creditPaymetn2Year = service.calculate(1_000_000, 9.99, 2 * 12);
         System.out.println("ежемесячный платеж по кредиту на 1 000 000 руб. сроком на 1 год " + creditPaymetn2Year + " руб под " + percentPerYear + "% годовых");
 
-        long creditPaymetn3Year = service.calculate(1_000_000, 9.99,  3 * 12);
+        long creditPaymetn3Year = service.calculate(1_000_000, 9.99, 3 * 12);
         System.out.println("ежемесячный платеж по кредиту на 1 000 000 руб. сроком на 1 год " + creditPaymetn3Year + " руб под " + percentPerYear + "% годовых");
     }
-    }
+}
